a822ff5a80e100c5687a332d7aa075d7efc22316
dianaMess <ospanova2000@mail.ru>
2019-05-21 19:56:54 +0600

    delete unnecessary code

--- crawler/crowler.cpp
+++ crawler/crowler.cpp
@@ -5,6 +5,7 @@
 //#include<regex>
 #include<string>
 #include<bits/stdc++.h>
+#include<fstream>
 using namespace std;
 
 size_t save_data(void *ptr, size_t size, size_t nmemb, void *userdata) {
@@ -29,19 +30,15 @@ int main(void) {
                   curl_easy_setopt(curl, CURLOPT_URL, "http://msu.kz");
               else 
                   curl_easy_setopt(curl, CURLOPT_URL, tmp1[j].c_str());
-//    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
               curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, save_data);
               curl_easy_setopt(curl, CURLOPT_WRITEDATA, &info); 
               res = curl_easy_perform(curl);
               if (res != CURLE_OK)
                   fprintf(stderr, "curl_easy_perform() failed: %s\n",
                   curl_easy_strerror(res));
-//              if (i != 0)
-//                  cout<<" ** " <<tmp1[j]<<endl;
               for (sregex_iterator it = sregex_iterator(info.begin(), info.end(), rgx); it != sregex_iterator(); it++) {
                   smatch match;
                   match = *it;
-//                  cout<<match.str(0)<<endl;
                   tmp.push_back(match.str(0));
                   list.push_back(match.str(0));
               }
@@ -53,7 +50,10 @@ int main(void) {
       }
   }
   curl_easy_cleanup(curl);
+  ofstream file;
+  file.open("answer.txt");
   for (int i = 0; i < list.size(); i++)
-      cout<<list[i]<<endl;;
+      file<<list[i]<<endl;;
+  file.close();
   return 0;
 }
